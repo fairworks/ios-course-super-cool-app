cb2d3bb0bedf8ca19553d9ac94856856067609b4
Luke <ohadattias@Ohads-MacBook-Air-2.local>
2016-06-11 19:06:19 -0400

    added files for testapp

---
+++ quiz.playground/Contents.swift
@@ -0,0 +1,33 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var firstvar = 15
+
+var secondvar = 5
+
+var sum = 3*15
+
+var sum2 = firstvar-secondvar
+
+var sum3 = firstvar/secondvar
+
+var new1 = 1, name2 = 3, name3 = 3
+
+let new4 = 3, new5 = 4, new6 = 5
+
+var new8: Double
+
+var new9: Int = 423
+
+var ser = "hello"
+
+var str3 = "world"
+
+var str10 = firstvar+secondvar
+
+str3 = "\(firstvar) \(secondvar)"
+
+
---
+++ var.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+var bubu = "hell"
+
+var david = 4.2
+
+david = 3.2
+
+print(david)
+
+let newtest = 4.234
+
